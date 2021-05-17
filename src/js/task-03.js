92418a4eeb6a99622f8762ebda18ee40009ec176
AntonTymchenko <76427403+AntonTymchenko@users.noreply.github.com>
2021-05-17 08:33:00 +0300

    fixed some things

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,25 +1,45 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
-const makeGalleryCollection = images => {
-  const arrayOfItem = images.map(item => {
+const makeGalleryCollection = (images) => {
+  const arrayOfItem = images.map((item) => {
     return `<li  class='gallary-item'><image src='${item.url}' alt='${item.alt}' width = 320 height = 200></li>`;
   });
   return arrayOfItem;
 };
 const arrayOfItems = makeGalleryCollection(images);
 
-const gallaryListEl = document.querySelector('ul#gallery');
+const gallaryListEl = document.querySelector("ul#gallery");
 
-gallaryListEl.insertAdjacentHTML('beforeend', arrayOfItems);
+gallaryListEl.insertAdjacentHTML("beforeend", arrayOfItems);
+
+//===== не через шаблонную строку ======
+// const galleryList = document.querySelector("ul#gallery");
+
+// const arrayOfItemsGallary = images.map((image) => {
+//   const itemOfGalleryListEl = document.createElement("li");
+//   itemOfGalleryListEl.classList.add("gallary-item");
+
+//   const imageOfItemGalleryEl = document.createElement("img");
+//   imageOfItemGalleryEl.src = image.url;
+//   imageOfItemGalleryEl.alt = image.alt;
+//   imageOfItemGalleryEl.width = 320;
+//   imageOfItemGalleryEl.height = 200;
+
+//   itemOfGalleryListEl.appendChild(imageOfItemGalleryEl);
+
+//   return itemOfGalleryListEl;
+// });
+
+// galleryList.append(...arrayOfItemsGallary);
