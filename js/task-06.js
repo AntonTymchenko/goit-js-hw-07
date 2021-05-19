0d1e1ef9b990031d0d04fe3495c4ea2ee6c0a287
AntonTymchenko <76427403+AntonTymchenko@users.noreply.github.com>
2021-05-19 13:31:04 +0300

    task 7 after review

--- js/task-06.js
+++ js/task-06.js
@@ -6,13 +6,16 @@ function onInputBlur(event) {
   if (event.currentTarget.value.length < input.getAttribute("data-length")) {
     input.classList.remove("valid");
     input.classList.add("invalid");
+    return;
   }
-  if (event.currentTarget.value.length >= input.getAttribute("data-length")) {
+  if (event.currentTarget.value.length === +input.getAttribute("data-length")) {
     input.classList.remove("invalid");
     input.classList.add("valid");
+    return;
   }
   if (event.currentTarget.value.length === 0) {
     input.classList.remove("invalid");
     input.classList.remove("valid");
+    return;
   }
 }
