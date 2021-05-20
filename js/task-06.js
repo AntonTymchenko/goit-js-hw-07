118565822f794870fa49ed2657aa88bf2e73948a
AntonTymchenko <76427403+AntonTymchenko@users.noreply.github.com>
2021-05-20 09:05:35 +0300

    fixed 7

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const input = document.querySelector("input#validation-input");
 input.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
-  if (event.currentTarget.value.length < input.getAttribute("data-length")) {
+  if (event.currentTarget.value.length !== +input.getAttribute("data-length")) {
     input.classList.remove("valid");
     input.classList.add("invalid");
     return;
@@ -11,11 +11,6 @@ function onInputBlur(event) {
   if (event.currentTarget.value.length === +input.getAttribute("data-length")) {
     input.classList.remove("invalid");
     input.classList.add("valid");
-    return;
-  }
-  if (event.currentTarget.value.length === 0) {
-    input.classList.remove("invalid");
-    input.classList.remove("valid");
     return;
   }
 }
