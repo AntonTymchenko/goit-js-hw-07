48277ffe2fc2fba495e4372f2ce754b8e933dc3a
AntonTymchenko <76427403+AntonTymchenko@users.noreply.github.com>
2021-05-20 09:10:58 +0300

    changes 7 tasks

--- js/task-06.js
+++ js/task-06.js
@@ -3,14 +3,16 @@ const input = document.querySelector("input#validation-input");
 input.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
-  if (event.currentTarget.value.length !== +input.getAttribute("data-length")) {
-    input.classList.remove("valid");
-    input.classList.add("invalid");
-    return;
-  }
   if (event.currentTarget.value.length === +input.getAttribute("data-length")) {
     input.classList.remove("invalid");
     input.classList.add("valid");
     return;
   }
+  if (event.currentTarget.value.length === 0) {
+    input.classList.remove("invalid");
+    input.classList.remove("valid");
+    return;
+  }
+  input.classList.remove("valid");
+  input.classList.add("invalid");
 }
